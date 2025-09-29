aae039499f6206b17247a58257fee4f2e29ae7d3
Bertucci Emil Federico <e.f.bertucci@gmail.com>
2025-09-29 18:17:46 +0200

    Created ApiQuestion.java to store the response json.

---
+++ src/ApiQuestion.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class ApiQuestion{
+
+    public String category;
+    public String type;
+    public String difficulty;
+    public String question;
+    public String correctAnswer;
+    public List<String> wrongAnswers;
+
+}
